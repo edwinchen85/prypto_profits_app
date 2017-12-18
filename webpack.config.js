2639825c05de35fa3413ebfde5d2178bcfd8dcd9
edwinchen85 <edwinchen85@gmail.com>
2017-12-19 03:28:20 +0800

    Add starter files

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack')
+
+const VENDOR_LIBS = [
+  'redux', 'react-redux', 'react-dom'
+]
+
+module.exports = {
+    entry: {
+        bundle: './assets/js/components/App.js',
+        vendor: VENDOR_LIBS
+        },
+    output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'public/js/components')
+        },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                presets: [
+                    [ 'es2015', { modules: false } ]
+                ]
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader", options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "sass-loader", options: {
+                        sourceMap: true
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        })
+    ]
+};
